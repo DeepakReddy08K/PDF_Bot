19906111fdba34fe923728f98e0ce43c27335727
DeepakReddy08K <kondakindideepakreddy@gmail.com>
2026-09-24 18:29:55 +0530

    Extract the data using pdf-parse and also delete the file after extracting to save the space

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,40 @@
 import express from "express";
 import multer from "multer";
+import fs from "fs";
+import { PDFParse } from 'pdf-parse';
+
 const app=express();
 const port=3000;
 const upload = multer({ dest: 'uploads/' })
+let fileName;
 app.get("/",(req,res)=>{
     console.log("Got a get request");
     res.send("<h1>Hello</h1>");
 });
-app.post('/upload', upload.single('pdf'),  (req, res)=> {
-  console.log(req.file);
-  res.json({
-    message:"PDF uploaded successfully",
-    filename:req.file.originalname
-  });
+app.post('/upload', upload.single('pdf'),  async(req, res)=> {
+  try{
+    fileName=req.file.path;
+    const parser = new PDFParse({url:fileName});
+
+	const pdfData = await parser.getText();
+	console.log(pdfData.text);
+    res.json({
+        message: "PDF processed successfully",
+        filename: req.file.originalname,
+        pages: pdfData.numpages,
+        text: pdfData.text
+    });
+    await parser.destroy();
+  }catch(error){
+    console.error(error);
+    res.status(500).json({ error: "Failed to process PDF" });
+  }finally{
+    //Delete the file after extracting data
+    if(fileName && fs.existsSync(fileName)){
+        fs.unlinkSync(fileName);
+    }
+  }
 });
 app.listen(port,()=>{
-    console.log("Server running on port $",{port});
+    console.log(`Server running on port ${port}`);
 });
